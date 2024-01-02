23f2ba3046c600e238ab8fb8b8d36d8b3b0fc527
Harsh Viradiya <viradiyaharsh70@gmail.com>
2024-01-02 12:54:37 +0530

    first command line

---
+++ mongoDB install.c
@@ -0,0 +1,16 @@
+//mongoDB install
+https://hevodata.com/learn/mongodb-compass-ubuntu/ 
+
+//chrome download
+https://linuxhint.com/install-google-chrome-on-ubuntu-22-04/
+
+//vs code
+https://phoenixnap.com/kb/install-vscode-ubuntu
+
+//curl install
+https://www.cyberciti.biz/faq/how-to-install-curl-command-on-a-ubuntu-linux/
+
+//github
+https://www.digitalocean.com/community/tutorials/how-to-install-git-on-ubuntu-20-04
+
+
